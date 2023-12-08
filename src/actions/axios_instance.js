c91fa89d7c7fe8bc11b1d15367ed7ff33dc299cd
wcorona269 <wcorona269@gmail.com>
2023-12-07 23:40:19 -0500

    add application/json formatting to axios requests

--- src/actions/axios_instance.js
+++ src/actions/axios_instance.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-	baseURL: process.env.REACT_APP_BACKEND_URL
+	baseURL: process.env.REACT_APP_BACKEND_URL,
+	headers: {
+		'Accept': 'application/json',
+		'Content-Type': 'application/json',
+	}
 });
 
 export default axiosInstance;
