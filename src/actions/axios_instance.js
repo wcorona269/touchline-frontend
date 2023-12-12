c1cf6f5ec06e9f7fde1abeb6ea0130ceca77fb91
wcorona269 <wcorona269@gmail.com>
2023-12-12 00:30:47 -0500

    fix auth fields and add backend link to axios

--- src/actions/axios_instance.js
+++ src/actions/axios_instance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-	// baseURL: "https://touchline-backend.azurewebsites.net/",
+	baseURL: "https://touchline-backend.azurewebsites.net/",
 	headers: {
 		'Accept': 'application/json',
 		'Content-Type': 'application/json',
